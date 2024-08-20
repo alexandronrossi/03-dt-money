20af7c0f932763b0d62c5a52f093bcb6bb834c00
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-20 20:05:56 -0300

    Curso: Consumo de API e permormance no ReactJS | Módulo: Estrutura visual | Aula: Botões de entrada e saída

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
-import { X } from 'phosphor-react';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
-import { CancelButton, Content, Overlay } from './styles'
+import { CancelButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -18,6 +18,18 @@ export function NewTransactionModal() {
 				<input type="text" placeholder='Descrição' required />
 				<input type="number" placeholder='Preço' required />
 				<input type="text" placeholder='Categoria' required />
+
+				<TransactionType>
+					<TransactionTypeButton variant='income'>
+						<ArrowCircleUp size={24} />
+						Entrada
+					</TransactionTypeButton>
+
+					<TransactionTypeButton variant='outcome'>
+						<ArrowCircleDown size={24} />
+						Saída
+					</TransactionTypeButton>
+				</TransactionType>
 
 				<button type="submit">Cadastrar</button>
 			</form>
