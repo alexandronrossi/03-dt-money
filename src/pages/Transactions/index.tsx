7fa79393e03c404515aa151418e21ae8164771f0
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-20 23:55:52 -0300

    Curso: Consumo de API e permormance no ReactJS | Módulo: Conexão com a API | Aula: Criando contexto de transações~

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+
 import {
 	PriceHighlight,
 	TransactionsContainer,
@@ -7,29 +9,10 @@ import {
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
-import { useEffect, useState } from "react";
-
-interface Transaction {
-	id: number;
-	description: string;
-	type: "income" | "outcome";
-	category: string;
-	price: number;
-	createdAt: string;
-}
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
-	const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-	async function loadTransactions() {
-		const response = await fetch("http://localhost:3000/transactions");
-		const data = await response.json();
-		setTransactions(data);
-	}
-
-	useEffect(() => {
-		loadTransactions();
-	}, []);
+	const { transactions } = useContext(TransactionsContext);
 
 	return (
 		<div>
