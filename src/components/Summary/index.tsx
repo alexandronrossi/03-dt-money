43e8fdf4121abd2261c2ef82f53bab577b67112a
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-21 00:03:54 -0300

    Curso: Consumo de API e permormance no ReactJS | Módulo: Conexão com a API | Aula: Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,6 +9,23 @@ export function Summary() {
 	const { transactions } = useContext(TransactionsContext);
 	console.log(transactions)
 
+	const summary = transactions.reduce(
+		(acc, transaction) => {
+			if (transaction.type === 'income') {
+				acc.income += transaction.price;
+				acc.total += transaction.price;
+			} else {
+				acc.outcome += transaction.price;
+				acc.total -= transaction.price;
+			}
+
+			return acc;
+		}, 
+		{ 
+			income: 0, outcome: 0, total: 0 
+		}
+	)
+
 	return (
 	  <SummaryContainer>
 		<SummaryCard>
@@ -17,7 +34,7 @@ export function Summary() {
 				<ArrowCircleUp size={32} color='#00B37E' />
 			</header>
 
-			<strong>R$ 17.400,00</strong>
+			<strong>{summary.income}</strong>
 		</SummaryCard>
 
 		<SummaryCard>
@@ -26,7 +43,7 @@ export function Summary() {
 				<ArrowCircleDown size={32} color='#F75A68' />
 			</header>
 
-			<strong>R$ 1.259,00</strong>
+			<strong>{summary.outcome}</strong>
 		</SummaryCard>
 
 		<SummaryCard variant='green'>
@@ -35,7 +52,7 @@ export function Summary() {
 				<CurrencyDollar size={32} color='#FFFFFF' />
 			</header>
 
-			<strong>R$ 16.141,00</strong>
+			<strong>{summary.total}</strong>
 		</SummaryCard>
 	  </SummaryContainer>
 	)
