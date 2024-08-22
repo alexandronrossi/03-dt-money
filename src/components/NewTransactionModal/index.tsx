e3c28b89c1ae9822725392f2001088fc21f998a2
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-21 21:06:30 -0300

    Curso: Consumo de API e permormance no ReactJS | Módulo: Conexão com a API | Aula: Controlled components

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
-import * as zod from 'zod';
-import { useForm } from "react-hook-form";
+import * as zod from "zod";
+import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
@@ -16,23 +16,27 @@ const newTransactionFormSchema = zod.object({
 	description: zod.string(),
 	price: zod.number(),
 	category: zod.string(),
-	type: zod.enum(['income', 'outcome']),
+	type: zod.enum(["income", "outcome"]),
 });
 
 type newTransactionFormInputs = zod.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
 	const {
+		control,
 		register,
 		handleSubmit,
-		formState: { isSubmitting }
+		formState: { isSubmitting },
 	} = useForm<newTransactionFormInputs>({
 		resolver: zodResolver(newTransactionFormSchema),
+		defaultValues: {
+			type: 'income'
+		}
 	});
 
 	async function handleCreateNewTransaction(data: newTransactionFormInputs) {
-		await new Promise(resolve => setTimeout(resolve, 2000))
-		console.log(data)
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		console.log(data);
 	}
 
 	return (
@@ -45,42 +49,58 @@ export function NewTransactionModal() {
 					<X size={24} />
 				</CancelButton>
 
-				<form action="" onSubmit={handleSubmit(handleCreateNewTransaction)}>
-					<input 
-						type="text" 
-						placeholder="Descrição" 
-						required 
-						{...register('description')}
+				<form
+					action=""
+					onSubmit={handleSubmit(handleCreateNewTransaction)}
+				>
+					<input
+						type="text"
+						placeholder="Descrição"
+						required
+						{...register("description")}
 					/>
-					<input 
-						type="number" 
-						placeholder="Preço" 
-						required 
-						{...register('price', { valueAsNumber: true })}
+					<input
+						type="number"
+						placeholder="Preço"
+						required
+						{...register("price", { valueAsNumber: true })}
 					/>
-					<input 
-						type="text" 
-						placeholder="Categoria" 
-						required 
-						{...register('category')}
+					<input
+						type="text"
+						placeholder="Categoria"
+						required
+						{...register("category")}
 					/>
 
-					<TransactionType>
-						<TransactionTypeButton variant="income" value="income">
-							<ArrowCircleUp size={24} />
-							Entrada
-						</TransactionTypeButton>
+					<Controller
+						control={control}
+						name="type"
+						render={({ field }) => {
+							return (
+								<TransactionType onValueChange={field.onChange} value={field.value}>
+									<TransactionTypeButton
+										variant="income"
+										value="income"
+									>
+										<ArrowCircleUp size={24} />
+										Entrada
+									</TransactionTypeButton>
 
-						<TransactionTypeButton
-							variant="outcome"
-							value="outcome"
-						>
-							<ArrowCircleDown size={24} />
-							Saída
-						</TransactionTypeButton>
-					</TransactionType>
+									<TransactionTypeButton
+										variant="outcome"
+										value="outcome"
+									>
+										<ArrowCircleDown size={24} />
+										Saída
+									</TransactionTypeButton>
+								</TransactionType>
+							);
+						}}
+					/>
 
-					<button type="submit" disabled={isSubmitting}>Cadastrar</button>
+					<button type="submit" disabled={isSubmitting}>
+						Cadastrar
+					</button>
 				</form>
 			</Content>
 		</AlertDialog.Portal>
