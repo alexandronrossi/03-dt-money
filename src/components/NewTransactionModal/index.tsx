4b02d3b9e6f6b4b6d6b6e4ffa6cad96649bec296
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-25 17:53:54 -0300

    Curso: Consumo de API e permormance no ReactJS | Módulo: Conexão com a API | Aula: Criando nova transação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,9 @@ import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 import * as zod from "zod";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from 'react';
+
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 import {
 	CancelButton,
@@ -22,11 +25,14 @@ const newTransactionFormSchema = zod.object({
 type newTransactionFormInputs = zod.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+	const { createTransaction } = useContext(TransactionsContext);
+
 	const {
 		control,
 		register,
 		handleSubmit,
 		formState: { isSubmitting },
+		reset,
 	} = useForm<newTransactionFormInputs>({
 		resolver: zodResolver(newTransactionFormSchema),
 		defaultValues: {
@@ -34,9 +40,10 @@ export function NewTransactionModal() {
 		}
 	});
 
-	async function handleCreateNewTransaction(data: newTransactionFormInputs) {
-		await new Promise((resolve) => setTimeout(resolve, 2000));
-		console.log(data);
+	async function handleCreateNewTransaction(data: newTransactionFormInputs) {		
+		await createTransaction(data);
+
+		reset();
 	}
 
 	return (
