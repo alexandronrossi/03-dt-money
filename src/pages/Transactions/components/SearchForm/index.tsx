6d8908d9a6cebf79261bf5b3222ccf55bf236673
Alexandro do Nascimento Rossi <alexandronrossi@hotmail.com>
2024-08-25 17:20:20 -0300

    Curso: Consumo de API e permormance no ReactJS | Módulo: Conexão com a API | Aula: Buscando transações

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -2,6 +2,9 @@ import { MagnifyingGlass } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import * as zod from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from 'react';
+
+import { TransactionsContext } from '../../../../contexts/TransactionsContext';
 
 import { SearchFormContainer } from "./styles";
 
@@ -12,6 +15,8 @@ const searchFormSchema = zod.object({
 type SearchFormInputs = zod.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+	const { fetchTransactions } = useContext(TransactionsContext);
+
 	const { 
 		register, 
 		handleSubmit,
@@ -21,6 +26,8 @@ export function SearchForm() {
 	});
 
 	async function handleSearchTransactions(data: SearchFormInputs) {
+		await fetchTransactions(data.query)
+
 		await new Promise(resolve => setTimeout(resolve, 2000))
 		console.log(data)
 	}
